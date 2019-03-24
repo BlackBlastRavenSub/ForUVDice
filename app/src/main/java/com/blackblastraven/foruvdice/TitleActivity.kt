049ba583e85eee83d470b6621389b34f62553ebc
KOGUMA0608 <smallbear0608@gmail.com>
2019-03-24 15:20:57 +0900

    //一応最低限のデータベース動作確認は完了

--- app/src/main/java/com/blackblastraven/foruvdice/TitleActivity.kt
+++ app/src/main/java/com/blackblastraven/foruvdice/TitleActivity.kt
@@ -8,6 +8,7 @@ import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_title.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class TitleActivity : AppCompatActivity() {
 
@@ -25,11 +26,11 @@ class TitleActivity : AppCompatActivity() {
             val db = Room.databaseBuilder(
                 applicationContext,
                 AppDatabase::class.java, "database-name"
-            ).build()
-            val dicedata = DiceData(0, 0, 0)
-            GlobalScope.launch {
-                db.DiceDataDao().insertDiceData(dicedata)
-            }
+            ).allowMainThreadQueries()
+                .build()
+            val dicedata = DiceData(10, 1, 2)
+            //GlobalScope.launch {
+            //thread{db.DiceDataDao().insertDiceData(dicedata)}
             val output = db.DiceDataDao().getAll()
             for (item in output) {
                 Toast.makeText(applicationContext, "$item", Toast.LENGTH_LONG).show()
