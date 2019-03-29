9b088e00055b27b7671441d65261798ffadcc959
KOGUMA <smallbear0608@gmail.com>
2019-03-29 21:05:45 +0900

    //データベースから選択したデータのみを取り出すことに成功!!

--- app/src/main/java/com/blackblastraven/foruvdice/ConfidentialActivity.kt
+++ app/src/main/java/com/blackblastraven/foruvdice/ConfidentialActivity.kt
@@ -12,10 +12,21 @@ class ConfidentialActivity : AppCompatActivity() {
         setContentView(R.layout.activity_confidential)
 
         //動的にボタンを作成中(最適解かはまだ不明)
+        /*
         val buttonPack = Array<Button>(inputNumber) { Button(applicationContext) }
-        for (i in 0..inputNumber-1) {
-            buttonPack[i].setText("Button" + (i+1))
+        for (i in 0..inputNumber - 1) {
+            buttonPack[i].setText("Button" + (i + 1))
+            //buttonPack[i].layout(0, 0, 100, 50)
+            buttonPack[i].height = 5
+            buttonPack[i].width = 10
             setContentView(buttonPack[i])
         }
+  */
+        var button = Button(applicationContext)
+        button.text = "TEST!"
+        button.maxHeight = 50
+        button.height = 50
+        setContentView(button)
+
     }
 }
