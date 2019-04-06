86a115bc67a808c0d8f3ecb09abf7799b4deb4d3
KOGUMA <smallbear0608@gmail.com>
2019-04-06 11:47:18 +0900

    //データベースから選択したデータのみを取り出すことに成功!! //コアプログラム製作中・・・ //coroutines製作中・・・

--- app/src/main/java/com/blackblastraven/foruvdice/TitleActivity.kt
+++ app/src/main/java/com/blackblastraven/foruvdice/TitleActivity.kt
@@ -53,7 +53,8 @@ class TitleActivity : AppCompatActivity() {
             }
         }
     }
-    /*
+}
+/*
     handler.postを使った非同期処理、一応成功、ただし全ての処理を別スレッドに記述しているため、スレッド間通信とは呼べない。スレッドAがスレッドBを用意して、その中ですべての処理を行ってしまっている。スレッド間相互通信は出来ていない。
             //handler
             val handler = Handler()
@@ -76,29 +77,37 @@ class TitleActivity : AppCompatActivity() {
                 }
             }
      */
-    /*
-    handler.messageを使った非同期処理、上手く行かず。
-            //handler
-            val handler = Handler()
-            //スレッド間通信用のメッセージ
-            var message = Message.obtain()
-            //テスト用データ
-            var output = DiceData(-1, -1, -1, -1, -1, -1)
-            GlobalScope.launch(Dispatchers.IO) {
-                if (!db.DiceDataDao().existsCheck(0)) {
-                    thread { db.DiceDataDao().createDiceDaia(DiceData(0, 1, 6, 1, 16, 0)) }
-                }
-                output = db.DiceDataDao().searchFromId(0)
-                message.what = 1
-                message.obj = Integer.toString(output.number)
-                //送信
-                handler.sendMessage(message)
+/*
+handler.messageを使った非同期処理、上手く行かず。
+    //handler
+    val handler = Handler()
+    //スレッド間通信用のメッセージ
+    var message = Message.obtain()
+    //テスト用データ
+    var output = DiceData(-1, -1, -1, -1, -1, -1)
+    GlobalScope.launch(Dispatchers.IO) {
+        if (!db.DiceDataDao().existsCheck(0)) {
+            thread { db.DiceDataDao().createDiceDaia(DiceData(0, 1, 6, 1, 16, 0)) }
+        }
+        output = db.DiceDataDao().searchFromId(0)
+        message.what = 1
+        message.obj = Integer.toString(output.number)
+        //送信
+        handler.sendMessage(message)
+        handler.post {
+            //Toast.makeText(applicationContext, "${output.number}", Toast.LENGTH_LONG).show()
+        }
+        var data = ""
+        //受信&表示
+    }
+    Toast.makeText(applicationContext, message.obj as String, Toast.LENGTH_LONG).show()
+*/
+/*
+//一般的なThread?
+            val handler2 = Handler()
+            Thread(Runnable {
                 handler.post {
-                    //Toast.makeText(applicationContext, "${output.number}", Toast.LENGTH_LONG).show()
+                    //処理
                 }
-                var data = ""
-                //受信&表示
-            }
-            Toast.makeText(applicationContext, message.obj as String, Toast.LENGTH_LONG).show()
-     */
-}
+            }).start()
+ */
